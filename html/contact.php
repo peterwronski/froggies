9b180f23961d333f64797e48b21c9d8a24322116
Peter <wronski.peter@gmail.com>
2018-04-20 12:19:37 +0100

    Final v1

---
+++ html/contact.php
@@ -0,0 +1,27 @@
+<div data-position="left" data-offset="60" class="row notViewed animBlock">
+    <div class="col-xs-12 col-sm-12 col-lg-2" id="contact" style="border-right: 1px solid black"><h1>Contact</h1></div>
+
+    <div class="col-xs-12 col-sm-12 col-lg-5">
+        <h3>You can contact us via phone or online, using the details below:</h3>
+        <ul>
+            <li class="menu"><button type="button" class="btn btn-info"><a href="tel:07709 797735"><span class="glyphicon glyphicon-phone-alt"></span><b> Didier</b> - 07709 797735</a></button>
+                <button type="button" class="btn btn-info"><a href="tel:07763 580173"><span class="glyphicon glyphicon-phone-alt"></span><b> Dominique</b> - 07763 580173</a></button></li>
+
+            <li class="menu"><span class="glyphicon glyphicon-envelope" class="contact"></span><b> info@froggiesdining.co.uk</b></li>
+
+            <li class="menu"><span class="glyphicon glyphicon-globe"></span> <a href="http://www.froggiesdining.co.uk" class="contact">www.froggiesdining.co.uk</a></li>
+        </ul>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-lg-5">
+            <h3>Alternatively, you can visit us on social media!</h3>
+        <ul>
+            <li class="menu"><span class="fa fa-facebook"></span><a href="https://www.facebook.com/froggiesdining/" class="contact"> Facebook / FroggiesDining</a></li> <br/>
+            <li class="menu"><span class="fa fa-camera-retro fa-lg"></span><a href="https://www.instagram.com/froggiesdining/" class="contact"> Instagram / FroggiesDining</a></li> <br/>
+            <li class="menu"><span class="fa fa-youtube fa-lg"></span><a href="https://www.youtube.com/channel/UCOByDldwPT53ZjILfaBI_9A" class="contact">  YouTube / FroggiesDining</a></li>
+        </ul>
+
+
+    </div>
+
+</div>
---
+++ html/services.php
@@ -0,0 +1,129 @@
+<div data-position="left" data-offset="60" class="row notViewed animBlock">
+    <div class="col-xs-12 col-sm-12 col-lg-2" id="services" style="border-right: 1px solid black"><h1>Services</h1> </div>
+
+    <div class="col-xs-12 col-sm-12 col-lg-5">
+
+        <h2>We cater for</h2>
+
+        <ul class="custom_bullet">
+            <li class="menu">Private Fine Dining either at your home or rented location for groups of 2 – 25 guests</li>
+            <li class="menu">Finger-type or Cold Buffets for up to 100 guests</li>
+            <li class="menu">Cooking lessons in the privacy of your own home</li>
+            <li class="menu">Consultancy</li>
+        </ul>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-lg-5">
+
+        <h2>How it works</h2>
+
+        <ul class="custom_bullet">
+            <li class="menu"><a href="#contact">Get in touch with either Dominique or Didier </a>, and we will meet with you to discuss all aspects of your requirements (including budget and wine choices to complement your menu and any allergies) </li>
+            <li class="menu">We buy the ingredients following confirmation </li>
+            <li class="menu">We come to your home to cook and serve </li>
+            <li class="menu">We tidy up! </li>
+
+        </ul>
+
+    </div>
+
+
+</div>
+
+<div class="row">
+    <div data-position="left" data-offset="60" class="col-xs-12 col-sm-12 col-lg-10 col-lg-offset-2 notViewed animBlock" style="background-color:#c09d9d">
+        <h4 align="center">Our prices are bespoke for each event, depending on your requirements and budget. Feel free to <a href="#contact">Contact Us</a> to get a personalised quote. </h4>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-lg-10 col-lg-offset-2">
+        <h2>Sample Menu</h2>
+    </div>
+</div>
+
+
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-lg-5 col-lg-offset-2">
+        <h3>Canapes</h3>
+    <ul>
+        <li class="menu"><b>Crab cake</b> (with wasabi mayo)</li>
+        <li class="menu"><b>Home cured salmon</b> (blinis)</li>
+        <li class="menu"><b>Haggis bonbons</b> (with turnips and apple puree)</li>
+        <li class="menu"><b>Tamarind duck</b> (mini poppadums)</li>
+        <li class="menu"><b>Salmon or mackerel</b> rillettes  </li>
+        <li class="menu"><b>Brushetta</b> (meat, seafood or vegetarian)</li>
+        <li class="menu"><b>Goat cheese cake</b> (with red onion jam)</li>
+    </ul>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-lg-5">
+
+        <h3>Starters</h3>
+        <ul>
+            <li class="menu"><b>Crab & lobster bisque</b></li>
+            <li class="menu"><b>Home cured beetroot salmon</b> with pickled vegetables & crème fraiche</li>
+            <li class="menu"><b>Pan fried tiger prawns</b> with basmati, mango & papaya coulis & cashewnut dressing</li>
+            <li class="menu"><b>Haggis croquettes</b> with turnips & apple puree & wholegrain mustard sauce </li>
+            <li class="menu"><b>Slow cooked pork belly</b> with gooseberries & black pudding</li>
+            <li class="menu"><b>Melon carpaccio</b></li>
+            <li class="menu"><b>Mixed tomato tartar</b> with green tapenade, mozzarella & basil oil</li>
+        </ul>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-lg-5 col-lg-offset-2 ">
+        <h3>Main Courses</h3>
+        <ul>
+            <li class="menu"><b>Rock turbot</b> poached in red wine with braised cabbage, mash & black pudding </li>
+            <li class="menu"><b>Pan fried cod</b> with saffron risotto & chorizo </li>
+            <li class="menu"><b>Hake</b> with crushed lemon potatoes, rocket, pesto, tomatoes & caper fondue </li>
+            <li class="menu"><b>Highland venison</b> with beetroot, red cabbage marmalade, mushrooms, confit celeriac & chestnut jus </li>
+            <li class="menu"><b>Moroccan braised lamb</b> with crushed chickpeas, confit peppers, courgettes & tagine jus </li>
+            <li class="menu"><b>Braised beef cheeks</b> with roasted root vegetables, potato mousseline, sage beignets & horseradish</li>
+            <li class="menu"><b>Butternut squash risotto</b> with tomato fondue, asparagus & parmesan dentelle </li>
+        </ul>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-lg-5">
+        <h3>Desserts</h3>
+        <ul>
+            <li class="menu">An array of <b>mini crème brulees</b></li>
+            <li class="menu"><b>Steamed carrot & coconut pudding</b> with Grand Marnier sauce & ice cream</li>
+            <li class="menu"><b>Eton mess</b></li>
+            <li class="menu"><b>Chocolate tart</b> with red fruit marmalade & ice cream</li>
+            <li class="menu">Selection of <b>speciality cheeses</b></li>
+        </ul>
+
+    </div>
+
+</div>
+
+<div class="row">
+    <div data-position="left" data-offset="60" class="col-xs-12 col-sm-12 col-lg-10 col-lg-offset-2 animation-element notViewed animBlock" style="background-color:#c09d9d">
+
+        <h4 align="center">Didier and Dominique will personalise your menu for your own specific requirements <br/>
+        Just sit down, relax, enjoy and let the show begin!</h4>
+
+    </div>
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
